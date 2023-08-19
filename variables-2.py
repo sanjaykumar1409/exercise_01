c2a23f3132af8e6eec5c3ed972598de09e1f0248
sanjay kumar <141133625+sanjaykumar1409@users.noreply.github.com>
2023-08-19 22:25:38 +0530

    Create variables-2.py

---
+++ variables-2.py
@@ -0,0 +1,10 @@
+#takes the input from the user
+a = input("a: ")
+b = input("b: ")
+c=b#here c value takes the value of b
+b=a#here b value takes the value of a
+a=c#now a value is taken by c which is the indirect representation of b 
+#hence we get the answer
+
+print("a: " + a)
+print("b: " + b)
